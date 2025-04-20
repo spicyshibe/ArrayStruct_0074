a5892b5fffc268e91dffdff29b73bc47ed14abca
spicyshibe <rafierasyw@gmail.com>
2025-04-20 15:49:01 +0700

    membuat output data

--- struct_array.cpp
+++ struct_array.cpp
@@ -23,6 +23,16 @@ int main()
         cout << "masukkan alamat = ";
         cin >> mhs[i].alamat;
     }
+    cout << endl;
+    cout << "Menampilkan Data" << endl;
+    cout << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "NIM = " << mhs[i].nim << endl;
+        cout << "NAMA = " << mhs[i].nama << endl;
+        cout << "ALAMAT = " << mhs[i].alamat << endl; 
+    }
 
 
 }
