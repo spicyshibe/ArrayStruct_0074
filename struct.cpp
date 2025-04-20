ba5a6391ae67fda76844bcaec5d8b1af67be9488
spicyshibe <rafierasyw@gmail.com>
2025-04-20 16:14:24 +0700

    membuat object struct

--- struct.cpp
+++ struct.cpp
@@ -8,3 +8,24 @@ struct Mahasiswa
     string alamat;
 
 };
+
+int main()
+{
+    //membuat object struct
+    Mahasiswa mhs;
+
+    cout << "Masukkan nim = ";
+    cin >> mhs.nim;
+    cout << "Masukkan nama = ";
+    cin >> mhs.nama;
+    cout << "masukkan alamat = ";
+    cin >> mhs.alamat;
+
+    cout << endl;
+    cout << "Menampilkan Data" << endl;
+    cout << endl;
+
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat = " << mhs.alamat << endl;
+}
