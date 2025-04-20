e7a639314d1561aacf096915d1eb1f0144c9cfa6
spicyshibe <rafierasyw@gmail.com>
2025-04-20 15:09:33 +0700

    deklarasi array

--- array.cpp
+++ array.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
 using namespace std;
 
+//deklarasi array tanpa ukuran, wajib diisi datanya
+int nilai[] = {1, 2, 4, 7};
+//deklarasi array dengan ukuran dan di beri nilai awal
+int nilaiDua[3] = {1, 2, 3};
+//deklarasi array dengan ukuran, tanpa di beri nilai awal
+string nama[4];
