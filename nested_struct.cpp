ea45ff145836a51f3a7affe3ad0a0ee685e9c1a1
spicyshibe <rafierasyw@gmail.com>
2025-04-20 15:34:06 +0700

    nested struct done

--- nested_struct.cpp
+++ nested_struct.cpp
@@ -10,15 +10,40 @@ struct detailAlamat
 
 struct Mahasiswa
 {
-    string Nama;
+    string nama;
     string nim;
     detailAlamat alamat;
 
 };
 
-int main 
+int main()
 {
     // membuat object struct dalam array 
     Mahasiswa mhs[2];
     for (int i = 0; i < 2; i++)
-}
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "Masukan nim = ";
+        cin >> mhs[i].nim;
+        cout << "Masukan nama = ";
+        cin >> mhs[i].nama;
+        cout << "Masukan kota = ";
+        cin >> mhs[i].alamat.kota;
+        cout << "Masukan provinsi = ";
+        cin >> mhs[i].alamat.provinsi;
+    }
+
+    cout << endl;
+    cout << "Menampilkan Data" << endl;
+    cout << endl; 
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "NIM = " << mhs[i].nim << endl;
+        cout << "Nama = " << mhs[i].nama << endl;
+        cout << "Kota = " << mhs[i].alamat. kota << endl;
+        cout << "Provinsi =" << mhs[i].alamat.provinsi << endl;
+    }
+
+    }
+    
